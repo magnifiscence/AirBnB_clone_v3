43d8da64007f4357a726368103dc0087d9f2d1c9
tata <tdivine284@gmail.com>
2024-01-07 22:38:12 +0000

    task done

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""
-initialize the models package
-"""
+"""This module instantiates an object of class FileStorage"""
+from models.engine.file_storage import FileStorage
+import os
+from models.engine.db_storage import DBStorage
 
-from os import getenv
 
-
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
-    from models.engine.db_storage import DBStorage
+if os.getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
+    # storage.reload()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
